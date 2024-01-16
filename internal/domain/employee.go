e77be5721fe2f66fe2de34296b451432e1cc9e71
LucasDigitalHouse <lucas.masiero@digitalhouse.com>
2024-01-16 10:03:48 -0300

    feat: clear files

--- internal/domain/employee.go
+++
@@ -1,9 +0,0 @@
-package domain
-
-type Employee struct {
-	ID           int    `json:"id"`
-	CardNumberID string `json:"card_number_id"`
-	FirstName    string `json:"first_name"`
-	LastName     string `json:"last_name"`
-	WarehouseID  int    `json:"warehouse_id"`
-}
